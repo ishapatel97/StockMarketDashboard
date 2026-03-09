8a32c901cb7ff6e5161dfe362a8d8d896cd907e9
Isha Patel <Ishapatel.7956@gmail.com>
2026-03-08 23:37:40 -0400

    database.py updated to use environment variable for database URL. The hardcoded database URL has been removed and replaced with a call to `os.getenv("DATABASE_URL")` to retrieve the database URL from the environment variables. This allows for better security and flexibility when deploying the application in different environments.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5433/stock_dashboard"
+DATABASE_URL = os.getenv("DATABASE_URL")
+#"postgresql://stockmarket_db_6z4q_user:App1I3cu54KCLdVlYKalVFZlIr4v9uvO@dpg-d6n3u8450q8c73atq74g-a/stockmarket_db_6z4q"
+#"postgresql://postgres:1234@localhost:5433/stock_dashboard"
+
 
 engine = create_engine(DATABASE_URL)
 
